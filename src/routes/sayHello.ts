23156b2e15e0818e11c5a994359ee87a3dd7b6c4
Calum Muir <calum.muir@aiven.io>
2023-01-17 17:08:21 +0100

    remove twilio dependency

--- src/routes/sayHello.ts
+++ src/routes/sayHello.ts
@@ -1,14 +1,15 @@
 import express from "express"
-import twilio from "twilio"
 
 const router = express.Router()
 
-const VoiceResponse = twilio.twiml.VoiceResponse
+const helloHtml: string = `
+  <div>
+    <h1>Hello World!!</h1>
+  </div>
+`
 
 router.post('/', (req, res) => {
-  const response = new VoiceResponse()
-  response.say("Hello World from Twilio!!");
-  res.send(response.toString())
+  res.send(helloHtml)
 });
 
 export default router
